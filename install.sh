10dc5f806ce3853ced0ed39cf0528473d2324a1e
JeromeSiljanUTA <jerome.siljan@mavs.uta.edu>
2022-02-24 17:40:36 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-gcc main.c -o media-control
+gcc main.c -o media-control-sway
 
-sudo mv media-control /usr/bin/media-control
+sudo mv media-control /usr/bin/media-control-sway
 
 echo "You should remove these lines:"
 sudo -u $1 cat "/home/$1/.config/sway/config" | grep XF86AudioPlay
 sudo -u $1 cat "/home/$1/.config/sway/config" | grep XF86AudioNext
 sudo -u $1 cat "/home/$1/.config/sway/config" | grep XF86AudioPrev
 
-sudo -u $1 echo "bindsym --locked XF86AudioPlay exec media-control play-pause" >> "/home/$1/.config/sway/config"
-sudo -u $1 echo "bindsym --locked XF86AudioNext exec media-control next" >> "/home/$1/.config/sway/config"
-sudo -u $1 echo "bindsym --locked XF86AudioPrev exec media-control previous" >> "/home/$1/.config/sway/config"
+sudo -u $1 echo "bindsym --locked XF86AudioPlay exec media-control-sway play-pause" >> "/home/$1/.config/sway/config"
+sudo -u $1 echo "bindsym --locked XF86AudioNext exec media-control-sway next" >> "/home/$1/.config/sway/config"
+sudo -u $1 echo "bindsym --locked XF86AudioPrev exec media-control-sway previous" >> "/home/$1/.config/sway/config"
